417427c30becbc1c3401960c8fd2ec29be1bfd29
henryvankirk4 <henryvankirk28@jpiicardinals.org>
2025-12-01 15:31:12 -0600

    Update Lab03B0405st.java
    Changed float to double

--- Lab 3B 4-5/Lab03B0405st.java
+++ Lab 3B 4-5/Lab03B0405st.java
@@ -40,7 +40,7 @@ class Rational
    // Complete implementation required for the 80-Point Version.
 	public double getDecimal() 
    { 
-      return num/ ((float) den);
+      return num/ ((double) den);
    }
    
    // Complete implementation required for the 80-Point Version.
